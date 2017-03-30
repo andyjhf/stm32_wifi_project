3776b7cd52c0e5609dcbc5faeae250717f351ab7
andyjhf <andyjhf@sina.com>
2017-03-30 10:44:57 +0800

    fix warning for Error_Handler

--- ST/stm32f4xx_hal_msp.c
+++ ST/stm32f4xx_hal_msp.c
@@ -47,7 +47,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx_hal.h"
-
+#include "driver_utility.h"
 /** @addtogroup STM32F4xx_HAL_Driver
   * @{
   */
